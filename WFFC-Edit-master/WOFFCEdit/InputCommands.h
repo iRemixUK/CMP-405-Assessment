3af8da7bb94444ae927afc608f795fc356c9d121
Aaron <1900400@uad.ac.uk>
2023-03-09 11:43:52 +0000

    Initial commit

---
+++ WFFC-Edit-master/WOFFCEdit/InputCommands.h
@@ -0,0 +1,14 @@
+#pragma once
+
+struct InputCommands
+{
+	bool forward;
+	bool back;
+	bool right;
+	bool left;
+	bool rotRight;
+	bool rotLeft;
+	int mouse_X;
+	int mouse_Y;
+	bool mouse_LB_Down;
+};
